06591afb57c299716a6d40bdb3de32a3d5950f64
Kainar92 <kainar.ilyasov@gmail.com>
2019-06-05 21:40:58 +0600

    Updates

---
+++ precache-manifest.e99f0bf620cd0b92ff3904bde197a69c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdcb12d1111142bb7583",
+    "url": "/rocket/static/js/runtime~main.412b8a83.js"
+  },
+  {
+    "revision": "bcc8c4ae1cd2fc4a7d44",
+    "url": "/rocket/static/js/main.a1704f6c.chunk.js"
+  },
+  {
+    "revision": "6b7a8549d14891e00d91",
+    "url": "/rocket/static/js/2.1e7b24b2.chunk.js"
+  },
+  {
+    "revision": "bcc8c4ae1cd2fc4a7d44",
+    "url": "/rocket/static/css/main.0b6f6e14.chunk.css"
+  },
+  {
+    "revision": "41c7112312a57737491bad536c1eec97",
+    "url": "/rocket/index.html"
+  }
+];
